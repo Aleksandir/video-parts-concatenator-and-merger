e63e1b5300d1b28ae07f9c549e40e3f3b4f60858
Aleks B <aleksandir@me.com>
2023-12-12 22:41:17 +1030

    Add video processing script and related files

---
+++ README.md
@@ -0,0 +1,29 @@
+# Video Processing Script
+
+This Python script reads video files from a specified directory, sorts them based on the part numbers in their names, and concatenates them into a single video file.
+
+## Requirements
+
+The script requires Python 3 and the following Python packages:
+
+- moviepy
+
+You can install the required packages with the following command:
+
+```sh
+pip install -r requirements.txt
+```
+
+## Running the Script
+
+You can run the script with the following command:
+
+```sh
+python src/main2.py
+```
+
+The script reads the video files from the specified directory, sorts them, and concatenates them into a single video file. The output video file is saved as `final.mp4` in the current directory.
+
+## Video File Names
+
+The video files should have part numbers in their names in the format "FILENAME Pt 6.mp4", where "6" is the part number. The script sorts the video files based on these part numbers. The part number can be located anywhere in the file name, but it must be the last number before the `.mp4` extension.
---
+++ src/main2.py
@@ -0,0 +1,67 @@
+# Import the os and moviepy modules
+import os
+import re
+
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+
+# Define the directory path where the videos are stored
+DIR_PATH = "/Users/aleks/Documents/_GIthub_Repositories/video-processing-script/videos"
+
+
+def get_unique_filename(base_path, filename):
+    counter = 1
+    name, extension = os.path.splitext(filename)
+    new_filename = filename
+
+    while os.path.exists(os.path.join(base_path, new_filename)):
+        new_filename = f"{name}_{counter}{extension}"
+        counter += 1
+
+    return new_filename
+
+
+def get_video_files(dir_path):
+    return [
+        f
+        for f in os.listdir(dir_path)
+        if os.path.isfile(os.path.join(dir_path, f)) and f.endswith(".mp4")
+    ]
+
+
+def sort_video_files(video_files):
+    return sorted(
+        video_files,
+        key=lambda x: int(re.search(r"(\d+)\.mp4$", x).group(1))
+        if re.search(r"(\d+)\.mp4$", x) is not None
+        else 0,
+    )
+
+
+def create_video_clips(video_files, dir_path):
+    return [VideoFileClip(os.path.join(dir_path, file)) for file in video_files]
+
+
+def main():
+    video_files = get_video_files(DIR_PATH)
+
+    video_files = sort_video_files(video_files)
+
+    # Print the list of video files and their index for user verify order
+    for i, video in enumerate(video_files):
+        print(i, video)
+
+    proceed = input("\nProceed? (y/n): ")
+    if proceed == "y":
+        # Create a list of VideoFileClip objects for each video file
+        clips = create_video_clips(video_files, DIR_PATH)
+
+        if clips:
+            final_clip = concatenate_videoclips(clips)
+
+            # Write the output video file
+            base_path = "videos/concatinated_vidoes"
+            filename = "final.mp4"
+            unique_filename = get_unique_filename(base_path, filename)
+            final_clip.write_videofile(os.path.join(base_path, unique_filename))
+        else:
+            print("No video clips found.")
